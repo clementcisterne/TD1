6a504d044b75694674b611501f883ec796dab5d4
clementcisterne <lamouche444@hotmail.fr>
2016-05-15 19:44:49 +0200

    1

--- src/applicationCryptage.java
+++ src/applicationCryptage.java
@@ -1,36 +1,119 @@
 import java.security.*;
 import javax.crypto.*;
+import java.util.Scanner;
 
+/**
+ *  String string = new String(byte[] bytes, Charset charset);
+ * */
 public class applicationCryptage {
 
     /** Type de cryptage */
-    public String algo = "DES";
+    private String algorithm;
+
+    /** Le message à crytper */
+    private String messageACrypter;
 
     /** Le message crypté */
-    public String messageCrypte;
+    private String messageCrypte;
 
     /** Génerateur de cle */
-    public KeyGenerator key;
+    private KeyGenerator keyGenerator;
+
+    /** Cle de cryptage */
+    private SecretKey key;
 
     /** Cipher */
-    public static Cipher cipher;
+    private Cipher cipher;
 
-
-
-
-
-    /** Crypter une chaine de caractère */
-    public String traitement(String chaine)
-    {
-        // Genere la clé
-        this.key = new KeyGenerator();
-        this.cipher = new Cipher();
-
-        // Crypte la chaine
-        this.key.doFinal(chaine);
-
-        return this.messageCrypte;
+    private applicationCryptage(String algorithm, String message) {
+        this.algorithm = algorithm;
+        this.messageACrypter = message;
     }
 
+
+
+    /**
+     *  Crypter une chaine de caractère entré en paramètre
+     *  @ param message String
+     *  @ return messageCrypte String
+     * */
+    public String encrypt(String message) {
+
+        try
+        {
+            System.out.println("\nCryptage : ");
+            System.out.println("les message est : "+message);
+
+            // Genere la clé
+            this.keyGenerator = KeyGenerator.getInstance(this.algorithm);
+            this.keyGenerator.init(56); // On spécifie la longueur de la cle
+            this.key = this.keyGenerator.generateKey();
+            System.out.println("clé : "+this.key);
+
+            // Gestion cipher
+            this.cipher = Cipher.getInstance(this.algorithm);
+            this.cipher.init(Cipher.ENCRYPT_MODE,this.key);
+            System.out.println("cipher : "+this.cipher);
+
+            // Crypte la chaine
+            this.messageCrypte = new String(this.cipher.doFinal(message.getBytes()));
+            System.out.println("Crypter la chaîne : ok");
+            System.out.println(this.messageCrypte);
+
+            return this.messageCrypte;
+
+        } catch (Exception e)
+        {
+         return null;
+        }
+    }
+
+    /**
+     *  Decrypte le message entré en paramètre
+     *  @param message String
+     *  @return messageDecrypte String
+    * */
+    public String decrypt(String message) {
+        try
+        {
+            System.out.println("\nDécryptage : ");
+            System.out.println("les message est : "+message);
+
+            // On récupère la cle
+            System.out.println("clé : "+this.key);
+
+            this.cipher = Cipher.getInstance(this.algorithm);
+            this.cipher.init(Cipher.DECRYPT_MODE,this.key);
+            System.out.println("cipher : "+this.cipher);
+
+            String messageDecrypte = new String(this.cipher.doFinal(message.getBytes()));
+            System.out.println("Message décrypté : "+messageDecrypte);
+
+            if (messageDecrypte == this.messageACrypter) {
+                System.out.println("Décryptage : "+messageDecrypte);
+
+                return messageDecrypte;
+            }
+            else {return null;}
+        } catch (Exception e)
+        {
+            System.out.println(e);
+            return null;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Entrez la chaîne de caractère à crypter :");
+        String messageACrypter = input.next();
+
+        applicationCryptage test1 = new applicationCryptage("DES",messageACrypter);
+
+        test1.decrypt(test1.encrypt(messageACrypter));
+
+    }
 
 }
